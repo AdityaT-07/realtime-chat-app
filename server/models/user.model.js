e488af30b90d083b90158b25b731b1e96898a96d
Aditya Thakur <adityathakur6076@gmail.com>
2026-01-03 23:37:00 +0530

    timestamp added

---
+++ server/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose, { type } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fullName : {
+    type : String,
+    required : true,
+  },
+  email : {
+    type : String,
+    required : true,
+  },
+  password : {
+    type : String,
+    required : true,
+  },
+  avatar : {
+    public_id : String,
+    url : String
+  }
+},{timestamps : true})
+
+export const User =  mongoose.model('User',userSchema)

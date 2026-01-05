5c7654f300e2b1e96e1a309adc02c9413722354d
Aditya Thakur <adityathakur6076@gmail.com>
2026-01-05 15:29:54 +0530

    update profile route setup

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,6 +2,7 @@ import { catchAsyncError } from "../middlewares/catchAsyncError.middleware.js"
 import {User} from '../models/user.model.js'
 import { generateToken } from "../utils/jwtToken.js"
 import bcrypt from 'bcrypt'
+import {v2 as cloudinary} from 'cloudinary'
 
 export const signup = catchAsyncError(async (req,res,next)=>{
     const{fullName,email,password} = req.body
@@ -96,7 +97,77 @@ export const signout = catchAsyncError(async (req,res,next)=>{
 })
 export const getUser = catchAsyncError(async (req,res,next)=>{
 
+    const user = req.user;
+    res.status(201).json({
+        success : true,
+        user
+    })
 })
 export const updateProfile = catchAsyncError(async (req,res,next)=>{
+
+    const {fullName, email} = req.body;
+
+    if(fullName.trim().length===0 || email.trim().length===0){
+        return res.status(400).json({
+            success : false,
+            message : "fullName and Email cannot be empty !",
+        })
+    }
+    const avatar = req?.files?.avatar;
+    let cloudinaryResponse = {};
+
+    if(avatar){
+
+        try {
+            const oldAvatarPublicId = req.user?.avatar?.public_id;
+            if(oldAvatarPublicId && oldAvatarPublicId.length>0){
+                await cloudinary.uploader.destroy(oldAvatarPublicId)
+            }
+             cloudinaryResponse = await cloudinary.uploader.upload(avatar.tempFilePath,{
+                folder : 'chat_app_user_avatar',
+                transformation : [
+                    {
+                        width : 300,
+                        height : 300,
+                        crop : 'limit'
+                    },
+                    {
+                        quality : 'auto'
+                    },
+                    {
+                        fetch_format : 'auto'
+                    }
+                ]
+             })
+        } catch (error) {
+            console.error("Cloudinary error : ",error)
+            return res.status(500).json({
+                success : false,
+                message : 'failed to upload avatar, please try again later !'
+            })
+        }
+    }
+
+    let data = {
+        fullName,
+        email
+    }
+    if(avatar && cloudinaryResponse?.public_id && cloudinaryResponse?.secure_url){
+        data.avatar = {
+            public_id : cloudinaryResponse.public_id,
+            url : cloudinaryResponse.secure_url
+        }
+    }
+
+    let user = await User.findByIdAndUpdate(req.user._id,data,{
+        new :  true,
+        runValidators : true
+    })
+
+    res.status(200).json({
+        success : true,
+        message : "profile updated Successfully!",
+        user,
+    })
 
 })

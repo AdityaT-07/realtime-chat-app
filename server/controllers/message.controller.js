062093eeb01efc00fe658c7dc80199832f467995
Aditya Thakur <adityathakur6076@gmail.com>
2026-01-06 15:40:34 +0530

    gellAllUser function defined

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,5 +1,19 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.middleware.js"
+import { User } from "../models/user.model.js";
 
-export const getAllUsers = catchAsyncError(async (req,res,next)=>{})
-export const getMessages = catchAsyncError(async (req,res,next)=>{})
+export const getAllUsers = catchAsyncError(async (req,res,next)=>{
+    const userId = req.user._id;
+    
+    
+    const filteredUser =  await User.find({_id : {$ne : userId}}).select('-password');
+    
+    
+    res.status(200).json({
+        success : true,
+        user : filteredUser,
+    })
+})
+export const getMessages = catchAsyncError(async (req,res,next)=>{
+    
+})
 export const sendMessage = catchAsyncError(async (req,res,next)=>{})

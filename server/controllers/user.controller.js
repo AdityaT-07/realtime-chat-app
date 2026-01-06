e950587ddec6b1d8b9c74d15004404844ec60142
Aditya Thakur <adityathakur6076@gmail.com>
2026-01-06 14:52:18 +0530

    optional chaining implemented

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -107,7 +107,7 @@ export const updateProfile = catchAsyncError(async (req,res,next)=>{
 
     const {fullName, email} = req.body;
 
-    if(fullName.trim().length===0 || email.trim().length===0){
+    if(fullName?.trim().length===0 || email?.trim().length===0){
         return res.status(400).json({
             success : false,
             message : "fullName and Email cannot be empty !",

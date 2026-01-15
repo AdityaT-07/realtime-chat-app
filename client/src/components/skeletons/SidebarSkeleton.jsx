c24882075c6af348c91ecc0f0c72b91cea52749a
Aditya Thakur <adityathakur6076@gmail.com>
2026-01-15 15:51:08 +0530

    sidebar-skeleton setup

--- client/src/components/skeletons/SidebarSkeleton.jsx
+++ client/src/components/skeletons/SidebarSkeleton.jsx
@@ -1,7 +1,47 @@
-import { Users } from "lucide-react";
+import {  Users } from "lucide-react";
 
 const SidebarSkeleton = () => {
-  return <></>;
+ 
+  const skeletonContacts = Array(8).fill(null);
+
+ 
+ return <>
+    <aside className="h-full w-20 lg:w-72 border-r border-gray-200 flex flex-col transition-all duration-200">
+      {/* header */}
+    <div className="border-b border-gray-200 w-full p-5">
+        <div className="flex items-center gap-2">
+          <Users className="w-6 h-6 text-gray-700" />
+          <span className="font-medium text-gray-800">
+            Contacts
+          </span>
+          </div>
+        </div>
+
+
+      {/* skeletion contact */}
+
+      <div className="overflow-y-auto w-full py-3 ">
+        {
+          skeletonContacts.map((_,index)=>{
+            return(
+              <div  key ={index} className="w-full p-3 flex items-center gap-3 animate-pulse">
+                {/* avatar skeltion */}
+                <div className="relative mx-auto lg:mx-0">
+                  <div className="w-12 h-12 bg-gray-300 rounded-full"/>
+                </div>
+
+              {/* text skeleton for large screen only  */}
+
+
+              </div>
+            )
+          })
+        }
+      </div>
+
+
+    </aside>
+  </>;
 };
 
 export default SidebarSkeleton;
